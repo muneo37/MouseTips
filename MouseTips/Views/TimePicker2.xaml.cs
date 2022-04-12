89f6add4f8028b39af2e33d53c9650a42229642e
muneo37 <y.wai.ysuke0822@gmail.com>
2022-04-12 17:00:46 +0900

    UserControlで作成中。

--- MouseTips/Views/TimePicker2.xaml.cs
+++ MouseTips/Views/TimePicker2.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Media.Animation;
 
 namespace MouseTips.Views
 {
@@ -23,6 +13,12 @@ namespace MouseTips.Views
         public TimePicker2()
         {
             InitializeComponent();
+        }
+
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            var fadeIn = FindResource("storyboardFadeIn") as Storyboard;
+            fadeIn.Begin();
         }
     }
 }
