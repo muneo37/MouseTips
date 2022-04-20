df3f012a264b82d10df014660b6268052d0331f8
muneo37 <y.wai.ysuke0822@gmail.com>
2022-04-20 11:35:02 +0900

    範囲外を非表示にした。

--- MouseTips/Views/TimePicker2.xaml.cs
+++ MouseTips/Views/TimePicker2.xaml.cs
@@ -18,18 +18,13 @@ namespace MouseTips.Views
             InitializeComponent();
 
             HourList = new ObservableCollection<hourText>();
-            var hour0 = new hourText("00");
-            HourList.Add(hour0);
 
-            var hour1 = new hourText("01");
-            HourList.Add(hour1);
-
-            var hour2 = new hourText("02");
-            HourList.Add(hour2);
-
-            var hour3 = new hourText("03");
-            HourList.Add(hour3);
-
+            for (int n = 0; n < 24; n++)
+            {
+                var h = n.ToString("00");
+                var hourT = new hourText(h);
+                HourList.Add(hourT);
+            }
         }
 
         private void OnButtonClick(object sender, RoutedEventArgs e)
@@ -47,13 +42,7 @@ namespace MouseTips.Views
             HourList[3] = new hourText("02");
         }
 
-        private void SlideReset_Completed(object sender, EventArgs e)
-        {
-
-
-        }
-
-        public DependencyProperty HourListProperty = DependencyProperty.Register("HourList", typeof(ObservableCollection<hourText>), typeof(TimePicker2), new FrameworkPropertyMetadata(default(ObservableCollection<hourText>), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+        public readonly static DependencyProperty HourListProperty = DependencyProperty.Register("HourList", typeof(ObservableCollection<hourText>), typeof(TimePicker2), new FrameworkPropertyMetadata(default(ObservableCollection<hourText>), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
 
         public ObservableCollection<hourText> HourList
         {
@@ -61,15 +50,6 @@ namespace MouseTips.Views
             set { SetValue(HourListProperty, value); }
         }
 
-        private void Storyboard_Completed(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Storyboard_Completed_1(object sender, EventArgs e)
-        {
-
-        }
     }
 
     public class hourText
